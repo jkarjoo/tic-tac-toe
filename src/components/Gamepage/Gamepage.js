de413f3777f0fdacae302469a216259ec63ca841
Justin Karjoo <justin800k@gmail.com>
2020-07-22 22:12:49 -0500

    Added AI with minimax algo and fixed memory leak

--- src/components/Gamepage/Gamepage.js
+++ src/components/Gamepage/Gamepage.js
@@ -16,14 +16,26 @@ class Gamepage extends Component {
     cellsFilled: 0,
     gameOver: false,
     resultText: '',
+    aiMoveInProgress: false,
+  };
+  timer = null;
+
+  componentWillUnmount = () => {
+    if (this.timer) {
+      clearTimeout(this.timer);
+    }
   };
 
   newGameHandler = () => {
-    this.setState({
-      gameOver: false,
-      board: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-      cellsFilled: 0,
-    });
+    if (!this.state.aiMoveInProgress) {
+      this.setState({
+        gameOver: false,
+        board: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+        cellsFilled: 0,
+        playerTurn: 'player1',
+        aiMoveInProgress: false,
+      });
+    }
   };
 
   winCombos = [
@@ -57,6 +69,22 @@ class Gamepage extends Component {
     }
   }
 
+  miniMaxCheckForWin(board, player) {
+    let result = false;
+    this.winCombos.forEach((subArr) => {
+      let counter = 0;
+      subArr.forEach((index) => {
+        if (board[index] === player) {
+          counter++;
+        }
+      });
+      if (counter === 3) {
+        result = true;
+      }
+    });
+    return result;
+  }
+
   renderDraw = () => {
     this.setState((prevState) => {
       return {
@@ -81,17 +109,72 @@ class Gamepage extends Component {
     });
   };
 
-  getPlayerSymbol() {
-    return this.state.playerTurn === 'player1' ? 'X' : 'O';
-  }
+  emptyCells = (board) => {
+    return board.filter((cell) => cell !== 'X' && cell !== 'O');
+  };
+
+  aiPlay = (board, player) => this.minimax(board, player).index;
+
+  minimax = (board, player) => {
+    const human = 'X';
+    const ai = 'O';
+    let availableSpots = this.emptyCells(board);
+
+    if (this.miniMaxCheckForWin(board, ai)) {
+      return { score: 10 };
+    } else if (this.miniMaxCheckForWin(board, human)) {
+      return { score: -10 };
+    } else if (availableSpots.length === 0) {
+      return { score: 0 };
+    }
+
+    let moves = [];
+    for (let i = 0; i < availableSpots.length; i++) {
+      let move = {};
+      let currentIndex = availableSpots[i];
+      move.index = board[currentIndex];
+      board[currentIndex] = player;
+
+      if (player === ai) {
+        let result = this.minimax(board, human);
+        move.score = result.score;
+      } else {
+        let result = this.minimax(board, ai);
+        move.score = result.score;
+      }
+      board[currentIndex] = move.index;
+      moves.push(move);
+    }
+
+    let bestMove;
+    if (player === ai) {
+      let bestScore = -10000;
+      for (let i = 0; i < moves.length; i++) {
+        if (moves[i].score > bestScore) {
+          bestScore = moves[i].score;
+          bestMove = i;
+        }
+      }
+    } else {
+      let bestScore = 10000;
+      for (let i = 0; i < moves.length; i++) {
+        if (moves[i].score < bestScore) {
+          bestScore = moves[i].score;
+          bestMove = i;
+        }
+      }
+    }
+    return moves[bestMove];
+  };
 
   placeMove = (e) => {
-    let position = e.target.getAttribute('data-position');
-    let value = e.target.textContent;
-    if (!this.state.gameOver && value !== 'X' && value !== 'O') {
+    const value = e.target.textContent;
+    if (!this.state.aiMoveInProgress && !this.state.gameOver && value !== 'X' && value !== 'O') {
+      let position = e.target.getAttribute('data-position');
       const boardCopy = [...this.state.board];
-      boardCopy[position] = this.getPlayerSymbol();
       let currentPlayer = this.state.playerTurn;
+
+      boardCopy[position] = this.state.playerTurn === 'player1' ? 'X' : 'O';
 
       this.setState(
         (prevState) => {
@@ -102,9 +185,39 @@ class Gamepage extends Component {
             playerTurn: prevState.playerTurn === 'player1' ? 'player2' : 'player1',
           };
         },
-        () => this.checkForWin(currentPlayer)
+        () => {
+          this.checkForWin(currentPlayer);
+          if (this.props.ai && this.state.playerTurn === 'player2') {
+            this.setState({
+              aiMoveInProgress: true,
+            });
+            this.timer = setTimeout(this.aiMove, 1250);
+          }
+        }
       );
     }
+  };
+
+  aiMove = () => {
+    let position = this.aiPlay(this.state.board, 'O');
+    const boardCopy = [...this.state.board];
+    boardCopy[position] = 'O';
+    this.setState(
+      (prevState) => {
+        return {
+          ...prevState,
+          board: boardCopy,
+          cellsFilled: prevState.cellsFilled + 1,
+          playerTurn: prevState.playerTurn === 'player1' ? 'player2' : 'player1',
+        };
+      },
+      () => {
+        this.checkForWin('player2');
+        this.setState({
+          aiMoveInProgress: false,
+        });
+      }
+    );
   };
 
   render() {
